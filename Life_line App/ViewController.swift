698a38071393fb413eec0131ee4914fbf9657b22
utkarshmishra1 <112176434+utkarshmishra1@users.noreply.github.com>
2024-07-06 02:02:36 +0530

    Auto layout

--- Life_line App/ViewController.swift
+++ Life_line App/ViewController.swift
@@ -1,17 +1,22 @@
 //
 //  ViewController.swift
-//  Life_line App
+//  Destini-iOS13
 //
-//  Created by utkarsh mishra on 06/07/24.
+//  Created by Angela Yu on 08/08/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
 
